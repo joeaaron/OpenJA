2df56447760eff04e3c7b36f1494973ec24ea5d4
Aaron-PC\Aaron <pant333@163.com>
2017-11-24 17:23:49 +0800

    delete the specified format files

--- modules/cplusplus/file/file.cpp
+++ modules/cplusplus/file/file.cpp
@@ -9,7 +9,6 @@
 
 using namespace JA::CPLUSPLUS;
 
-
 int File::copyFile(const char * pSrc, const char *pDes)
 {
 	FILE *in_file, *out_file;
@@ -88,7 +87,6 @@ FILE_API int File::copyDir(const char* pSrc, const char* pDes)
 	}
 	return -3;
 }
-
 //************************************
 // Method:    countDirNum
 // FullName:  JA::CPLUSPLUS::File::countDirNum
@@ -132,4 +130,128 @@ FILE_API int File::countDirNum(const char* pSrc, int& dirNum)
 	}
 
 	return dirNum;
+}
+
+FILE_API void File::getAllFiles(string pSrcPath, vector<string>& files)
+{
+	long hFile = 0;
+	struct _finddata_t fileinfo;
+	string p;
+	if ((hFile = _findfirst(p.assign(pSrcPath).append("\\*").c_str(), &fileinfo)) != -1)
+	{
+		do
+		{	
+			//如果是文件夹就进入文件夹，迭代
+			if ((fileinfo.attrib &  _A_SUBDIR))
+			{		
+				if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0)
+				{
+					files.push_back(p.assign(pSrcPath).append("\\").append(fileinfo.name));
+					getAllFiles(p.assign(pSrcPath).append("\\").append(fileinfo.name), files);
+				}
+			}
+			else
+			{
+				files.push_back(p.assign(pSrcPath).append("\\").append(fileinfo.name));
+			}
+
+		} while (_findnext(hFile, &fileinfo) == 0);
+
+		_findclose(hFile);
+	}
+
+}
+
+//************************************
+// Method:    delete the specified format files
+// FullName:  JA::CPLUSPLUS::File::delAllFormatFiles
+// Access:    public static 
+// Returns:   FILE_API void
+// Qualifier:
+// Parameter: string path
+//************************************
+FILE_API void File::delAllFormatFiles(string path)
+{
+	long hFile = 0;
+	struct _finddata_t fileInfo;
+	string pathName, exdName;
+	// \\* 代表要遍历所有的类型
+	if ((hFile = _findfirst(pathName.assign(path).append("\\*").c_str(), &fileInfo)) == -1) {
+		cout << "error no file!" << endl;
+		return;
+	}
+	do
+	{
+		//判断文件的属性是文件夹还是文件
+		cout << fileInfo.name << (fileInfo.attrib&_A_SUBDIR ? "[folder]" : "[file]") << endl;
+		//如果是文件夹就进入文件夹，迭代
+		if (fileInfo.attrib&_A_SUBDIR) {
+			{//遍历文件系统时忽略"."和".."文件
+				if (strcmp(fileInfo.name, ".") != 0 && strcmp(fileInfo.name, "..") != 0) {
+					string tmp;
+					tmp = path + "\\" + fileInfo.name;
+					delAllFormatFiles(tmp);
+				}
+			}
+
+		}
+		//是文件的话就查看文件名，不是“bmp”就删除
+		else {
+			//delete file
+			if (strcmp(fileInfo.name, ".") != 0 && strcmp(fileInfo.name, "..") != 0) {
+				std::string fileSuffix(fileInfo.name);
+				fileSuffix = fileSuffix.substr(fileSuffix.find_last_of('.') + 1);		//获取文件后缀  
+				if (strcmp(fileSuffix.c_str(), "bmp")) {
+					string delpath = path + "\\" + fileInfo.name;
+					if (remove(delpath.c_str()) != 0)			//删除失败就报错
+						perror("Error deleting file");
+					else {
+						cout << fileInfo.name << "deleted" << endl;
+					}
+				}
+			}
+		}
+	} while (_findnext(hFile, &fileInfo) == 0);
+	_findclose(hFile);
+	return;
+}
+
+//************************************
+// Method:    getAllFormatFiles
+// FullName:  JA::CPLUSPLUS::File::getAllFormatFiles
+// Access:    public static 
+// Returns:   FILE_API void
+// Qualifier:
+// Parameter: string path
+// Parameter: vector<string> & files
+// Parameter: string format
+// From: http://blog.csdn.net/adong76/article/details/39432467
+//************************************
+FILE_API void File::getAllFormatFiles(string path, vector<string>& files, string format)
+{
+	//文件句柄    
+	long   hFile = 0;
+	//文件信息    
+	struct _finddata_t fileinfo;
+	string p;
+	if ((hFile = _findfirst(p.assign(path).append("\\*" + format).c_str(), &fileinfo)) != -1)
+	{
+		do
+		{
+			if ((fileinfo.attrib &  _A_SUBDIR))
+			{
+				if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0)
+				{
+					//files.push_back(p.assign(path).append("\\").append(fileinfo.name) );  
+					getAllFormatFiles(p.assign(path).append("\\").append(fileinfo.name), files, format);
+				}
+			}
+			else
+			{
+				files.push_back(p.assign(path).append("\\").append(fileinfo.name));
+			}
+		} while (_findnext(hFile, &fileinfo) == 0);
+
+		_findclose(hFile);
+	}
 }
--- samples/testFile.cpp
+++ samples/testFile.cpp
@@ -1,13 +1,37 @@
 #include "file.h"
-
+#include <vector>
+#include <io.h>
+#include <fstream>
+#include <string>
 using namespace JA::CPLUSPLUS;
 
 int main()
 {
 	//std::cout << "please wait ...";
 	//File::copyDir("C:\\Users\\Aaron\\Desktop\\ECAT-Vision-Team\\build\\src\\gui\\images", "D:\\calibFiles\\cowa_R1");
-	int num = 0 ;
+
+	/*int num = 0 ;
 	File::countDirNum("D:\\CalibFiles\\WUHU", num);
-	std::cout << num;
+	std::cout << num;*/
+
+	string filePath = "G:\\JA\\OpenJA\\samples\\data\\img";
+	vector<string> files;
+	//save the info to allfiles.txt
+	char * distAll = "AllFiles.txt";
+	//File::getAllFiles(filePath, files);
+	//读取所有格式为jpg的文件  
+
+	/*string format = ".bmp";
+	File::getAllFormatFiles(filePath, files, format);
+	ofstream ofn(distAll);
+	int size = files.size();
+	ofn << size << endl;
+	for (int i = 0; i < size; i++)
+	{
+		ofn << files[i] << endl;
+		std::cout << files[i] << endl;
+	}
+	ofn.close();*/
+	File::delAllFormatFiles(filePath);
 	return 0;
 }
